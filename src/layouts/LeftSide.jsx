52af0e19f6f43323eafdd43865869aedeef365b4
gokhangultan <gultangokhan@gmail.com>
2024-10-14 18:20:38 +0300

    mahmutbey check

--- src/layouts/LeftSide.jsx
+++ src/layouts/LeftSide.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function LeftSide() {
   
 
   return (
     <div className="w-[310px]">
-        1
+        <div className="flex flex-col gap-2">
+        <Link to="/users">users</Link>
+        <Link to="/edituser">editusers</Link>
+        <Link to="/usersroles">usersroles</Link>
+        <Link to="/library">Library</Link>
+        </div>
     </div>
   );
 }
